81fd5c53738571311c829f5e43f9870a70a6d0c7
Dhruv <vagadiyadhruv00@gmail.com>
2023-02-28 12:07:44 +0530

    Add project files.

---
+++ BulkyBook.Utility/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("vagadiyadhru00@gmail.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            using(var emailCliennt = new SmtpClient())
+            {
+                emailCliennt.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                emailCliennt.Authenticate("vagadiyadhru00@gmail.com", "irnkhzqqzfhmqoti");
+                emailCliennt.Send(emailToSend);
+                emailCliennt.Disconnect(true);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
